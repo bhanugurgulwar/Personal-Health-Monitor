db9ed15650376d23c24c18ac36a028a38ae768b1
bhanudas gurgulwar <bhanudasgurgulwar27@gmail.com>
2023-11-01 12:57:55 +0530

    added custom component

---
+++ client/src/app/shared/components/customcard/customcard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'customcard',
+  templateUrl: './customcard.component.html',
+  styleUrls: ['./customcard.component.scss'],
+})
+export class CustomcardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- client/src/app/tabs/Pages/dashboard/dashboard.module.ts
+++ client/src/app/tabs/Pages/dashboard/dashboard.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
 
 import { DashboardPage } from './dashboard.page';
+import { CustomcardComponent } from 'src/app/shared/components/customcard/customcard.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { DashboardPage } from './dashboard.page';
     IonicModule,
     DashboardPageRoutingModule
   ],
-  declarations: [DashboardPage]
+  declarations: [DashboardPage,CustomcardComponent]
 })
 export class DashboardPageModule {}
