ebddfc44ef02f93f245e00d60a056316e25c8391
nishant-angularminds <nishant.jarang@angularminds.in>
2023-11-02 12:24:15 +0530

    register page1 ui

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,17 +6,23 @@ import { LoginPageModule } from './pages/login/login.module';
 import { LoginPage } from './pages/login/login.page';
 
 const routes: Routes = [
-  
+
   {
 
-    path:'',
-    redirectTo:'auth/login',
-    pathMatch:'full'
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full'
   }
   ,
   {
-    path:'auth/login',
-    component:LoginPage
+    path: 'auth/login',
+    component: LoginPage
+  },
+
+  {
+
+    path:'auth/register',
+    component:RegisterPage
   }
 
 ];
