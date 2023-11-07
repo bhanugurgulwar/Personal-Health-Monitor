1f249be2d9d5d33be208dbadff78a767d7ae1271
SanketAM39 <Sanket@angularminds.in>
2023-11-07 14:34:02 +0530

    token service added

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,2 +1,3 @@
 module.exports.authService = require("../services/auth.service");
 module.exports.userService = require("../services/user.service");
+module.exports.tokenService = require("../services/token.service");
